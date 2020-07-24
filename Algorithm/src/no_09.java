a9f11ba3a50994bdb32a56cd9bef276c84b62a57
joo-chang <jooc031@naver.com>
2020-07-24 14:39:04 +0900

    789

---
+++ Algorithm/src/no_09.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// 입력된 수의 각 자릿수 합을 구하시오
+public class no_09 {
+	public static void main(String[] args) {
+		int num, sum=0;
+		Scanner sc = new Scanner(System.in);
+		
+		num = sc.nextInt();
+		
+		while(num>0) {
+			sum += num %10;
+			num /= 10;
+		}
+		System.out.println(sum);
+		
+	}
+}
