c018c7721ee589be1c07ad690d97b15add9be609
joochang <jooc0311@gmail.com>
2021-09-28 09:13:46 +0900

    no message

--- PythonWorkspace/Sorting.py
+++ PythonWorkspace/Sorting.py
@@ -55,3 +55,20 @@ def quick_sort(array, start, end):
 
 quick_sort(array, 0, len(array)-1)
 print(array)
+
+# 퀵 정렬 알고리즘 / 파이썬 장점
+
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
