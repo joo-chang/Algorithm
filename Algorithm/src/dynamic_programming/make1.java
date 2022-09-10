3b38af0c2d12ea6362be18da076dcf14b03c3673
박주창 <jooc0311@gmail.com>
2022-09-10 17:31:53 +0900

    1로 만들기 답안작성

--- Algorithm/src/dynamic_programming/make1.java
+++ Algorithm/src/dynamic_programming/make1.java
@@ -24,6 +24,24 @@ public class make1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int num = sc.nextInt();
 
+        int[] d = new int[30001];
+
+        for (int i = 2; i < d.length; i++) {
+            d[i] = d[i-1] + 1;
+
+            if (i % 2 == 0){
+                d[i] = Math.min(d[i], d[i/2] + 1);
+            }
+            if (i % 3 == 0){
+                d[i] = Math.min(d[i], d[i/3] + 1);
+            }
+            if (i % 5 == 0){
+                d[i] = Math.min(d[i], d[i/5] + 1);
+            }
+        }
+
+        System.out.println(d[num]);
     }
 }
