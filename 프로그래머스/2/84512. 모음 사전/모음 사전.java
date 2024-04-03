9448f3988ff9817ad9acfa5d1f3a87ccfae51475
Joochang Park <jooc0311@gmail.com>
2024-04-03 15:45:51 +0900

    [level 2] Title: 모음 사전, Time: 0.76 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,26 +1,53 @@
 import java.util.*;
 class Solution {
-    static String[] v = {"A", "E", "I", "O", "U"};
-    public int solution(String word) {
-        int answer = 0;
-        List<String> list = new ArrayList<>();
-        dfs(list, "", 0);
-        // for(int i = 0; i < list.size(); i++){
-        //     if(list.get(i).equals(word)){
-        //         answer = i;
-        //         break;
-        //     }
-        // }
-        return list.indexOf(word);
-    }
+//     static String[] v = {"A", "E", "I", "O", "U"};
+//     public int solution(String word) {
+//         int answer = 0;
+//         List<String> list = new ArrayList<>();
+//         dfs(list, "", 0);
+//         // for(int i = 0; i < list.size(); i++){
+//         //     if(list.get(i).equals(word)){
+//         //         answer = i;
+//         //         break;
+//         //     }
+//         // }
+//         return list.indexOf(word);
+//     }
     
-    public void dfs(List<String> list, String w, int depth){
-        list.add(w);
-        if(depth == 5) return;
+//     public void dfs(List<String> list, String w, int depth){
+//         list.add(w);
+//         if(depth == 5) return;
         
-        for(int i = 0; i < 5; i++){
-            dfs(list, w + v[i], depth + 1);
+//         for(int i = 0; i < 5; i++){
+//             dfs(list, w + v[i], depth + 1);
+//         }
+        
+//     }
+    
+    private StringBuilder sb = new StringBuilder();
+    private char[] v = {'A','E','I','O','U'};
+    private boolean flag = false;
+    private int answer = 0;
+
+    private void dfs(String word) {
+        if(word.equals(sb.toString())) {
+            flag = true;
+            return;
         }
-        
+        if(sb.length() == 5) return;
+
+        for(int i=0; i<5; i++) {
+            answer++;
+            sb.append(v[i]);
+            dfs(word);
+            sb.deleteCharAt(sb.length()-1);
+
+            if(flag) return;
+        }
+    }
+
+    public int solution(String word) {
+        dfs(word);
+        return answer;
     }
 }
