c25784dfe5f55f2457e3103f006464f5e7714cb2
Joochang Park <jooc0311@gmail.com>
2024-04-03 15:37:26 +0900

    [level 2] Title: 모음 사전, Time: 4.71 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -6,13 +6,7 @@ class Solution {
         int answer = 0;
         list = new ArrayList<>();
         dfs("", 0);
-        for(int i = 0; i < list.size(); i++){
-            if(list.get(i).equals(word)){
-                answer = i;
-                break;
-            }
-        }
-        return answer;
+        return list.indexOf(word);
     }
     
     public void dfs(String w, int depth){
