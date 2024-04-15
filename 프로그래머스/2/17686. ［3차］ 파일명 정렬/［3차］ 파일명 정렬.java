17715346f80d20372ac669f5f78438668dd5cc2b
Joochang Park <jooc0311@gmail.com>
2024-04-15 18:08:49 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 24.33 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    /*
+    소요 시간 : 60분
+    풀이 : head 찾기
+    HEAD 정렬, NUMBER 정렬
+    */
+    public String[] solution(String[] files) {
+        String[] answer = {};
+        Arrays.sort(files, (o1, o2) -> {
+            String[] arr1 = setFileName(o1);
+            String[] arr2 = setFileName(o2);
+            
+            if(arr1[0].toLowerCase().equals(arr2[0].toLowerCase())){
+                return Integer.parseInt(arr1[1]) - Integer.parseInt(arr2[1]);
+            }else{
+                return arr1[0].toLowerCase().compareTo(arr2[0].toLowerCase());
+            }
+        });
+        return files;
+    }
+    
+    String[] setFileName(String file){
+        String[] result = new String[3];
+        boolean flag = false;
+        for(int i = 0 ; i < file.length(); i++){
+            if(Character.isDigit(file.charAt(i))){
+                if(!flag){ // 처음으로 숫자가 나왔을 경우
+                    result[0] = file.substring(0, i);
+                    flag = true;
+                }
+                if(i == file.length() - 1){
+                    result[1] = ""+Integer.parseInt(file.substring(result[0].length()));
+                    break;
+                }
+            }else if(!Character.isDigit(file.charAt(i)) && flag){ // TAIL 부분
+                result[1] = ""+Integer.parseInt(file.substring(result[0].length(), i));
+                break;
+            }
+        }
+        return result;
+    }
+}
