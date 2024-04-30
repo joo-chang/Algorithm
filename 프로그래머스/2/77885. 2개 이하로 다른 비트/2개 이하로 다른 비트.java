83ed19aaf643b19a27f55c959fc14e15e5ba3e19
Joochang Park <jooc0311@gmail.com>
2024-04-30 11:07:12 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 15.86 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,16 +1,29 @@
 class Solution {
     /*
-    소요 시간 : 
-    풀이 : 가장 가까운 수는 현재 비트에서 오른쪽에서부터 0이 처음 나오는 위치를 1로 변경하는 것
-    0이 없을 경우 (이진수로 변경 후 길이 만큼 반복했을 때 0이 나오지 않았을 경우) 맨 앞자리
-    100001 - 100011
-    101111 - 111111
-    111111 - 1011111
-    
-    
+    소요 시간 : 50분
+    풀이 : 
+    짝수일 때 - 1의 자리 수가 0이므로 +1만 해주면 된다.
+    홀수일 때 - 2로 나눠가면서 0이 나오는 위치를 1로 바꿔주고 그 전 위치 값을 0으로 바꿔준다. (가장 작은 변동)
+    ex) 1011(11) + 100(4) - 10 (2) = 1101(13)
     */
     public long[] solution(long[] numbers) {
-        long[] answer = {};
+        long[] answer = new long[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] % 2 == 0){
+                answer[i] = numbers[i] + 1;
+                continue;
+            }
+            long num = numbers[i];
+            int n = 0;
+            while(true){
+                if(num % 2 == 0){
+                    answer[i] = numbers[i] + (long)Math.pow(2, n) - (long)Math.pow(2, n - 1);
+                    break;
+                }
+                num /= 2;
+                n++;
+            }
+        }
         return answer;
     }
 }
