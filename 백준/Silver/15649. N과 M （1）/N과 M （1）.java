4578851a47a2370c182ddc2e1a813a2ceead51ae
Joochang Park <jooc0311@gmail.com>
2024-06-27 10:17:21 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 20456 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] visited;
+    static int[] arr;
+    static int n;
+    static int m;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n];
+        arr = new int[m];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+    private static void dfs(int depth) {
+        if (depth == m) {
+            for (int a : arr) {
+                sb.append(a).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
