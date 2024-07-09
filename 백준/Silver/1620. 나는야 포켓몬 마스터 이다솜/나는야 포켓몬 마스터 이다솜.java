a58bb77b51787108de3de55f8679226f131b485a
Joochang Park <jooc0311@gmail.com>
2024-07-09 10:16:10 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1244 ms, Memory: 51096 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -10,20 +10,20 @@ class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Map<Integer, String> num = new HashMap<>();
         Map<String, Integer> name = new HashMap<>();
+        String[] num = new String[n + 1];
 
 
         for (int i = 1; i <= n; i++) {
             String s = bf.readLine();
             name.put(s, i);
-            num.put(i, s);
+            num[i] = s;
         }
 
         for (int i = 0; i < m; i++) {
             String s = bf.readLine();
             if(s.charAt(0) >= '0' && s.charAt(0) <= '9') {
-                System.out.println(num.get(Integer.parseInt(s)));
+                System.out.println(num[Integer.parseInt(s)]);
             } else {
                 System.out.println(name.get(s));
             }
