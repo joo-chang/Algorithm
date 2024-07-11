a27009e12829db746d60399e86c540c3b8161aef
Joochang Park <jooc0311@gmail.com>
2024-07-11 12:02:18 +0900

    [level 2] Title: 롤케이크 자르기, Time: 14.54 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,8 +3,12 @@ class Solution {
         int answer = 0;
         int[] left = new int[10001], right = new int[10001];
         int ls = 0, rs = 0;
+        // 오른쪽에 모든 값 세팅
         for(var i : topping) right[i]++;
+        // 토핑 종류
         for(var i : right) rs += i > 0 ? 1 : 0;
+        // 오른쪽 개수 줄이면서 왼쪽 개수 증가
+        // 0일 경우 왼쪽 토핑 증가
         for(var i : topping) {
             right[i]--;
             if (right[i] == 0) rs--;
