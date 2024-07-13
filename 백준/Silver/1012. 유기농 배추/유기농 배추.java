1ee58cef83f6ade575f3882db6f95eef2686294c
Joochang Park <jooc0311@gmail.com>
2024-07-13 09:32:56 +0900

    [Silver II] Title: 유기농 배추, Time: 92 ms, Memory: 13440 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,63 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+
     static int[][] arr;
-    static boolean[][] visited;
+
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(bf.readLine());
         for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int m = Integer.parseInt(st.nextToken());
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
-            int answer = 0;
-            arr = new int[n][m];
-            visited = new boolean[n][m];
+            arr = new int[m][n];
+            int bug = 0;
 
+            // arr 세팅
             for (int j = 0; j < k; j++) {
                 st = new StringTokenizer(bf.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-
-                arr[y][x] = 1;
+                arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
             }
+
+            // 모든 배열 방문하면서 dfs 수행
             for (int j = 0; j < m; j++) {
                 for (int l = 0; l < n; l++) {
-                    if (arr[l][j] == 1 && !visited[l][j]) {
-                        dfs(l, j);
-                        answer++;
+                    if(arr[j][l] == 1){
+                        dfs(j, l);
+                        bug++;
                     }
                 }
             }
-            System.out.println(answer);
-        }
 
+            sb.append(bug).append("\n");
+        }
+        System.out.println(sb);
     }
 
     private static void dfs(int y, int x) {
-        visited[y][x] = true;
+        // 방문 배열 대신 1을 0으로 변경
+        arr[y][x] = 0;
 
         for (int i = 0; i < 4; i++) {
             int ny = dy[i] + y;
             int nx = dx[i] + x;
 
-            if (check(ny, nx) || arr[ny][nx] == 0) continue;
-
-            if (!visited[ny][nx]) {
+            if (check(ny, nx)) continue;
+            if (arr[ny][nx] == 1){
                 dfs(ny, nx);
             }
         }
+
     }
 
-    private static boolean check(int y, int x) {
-        return y < 0 || x < 0 || y >= arr.length || x >= arr[0].length;
+    private static boolean check(int ny, int nx) {
+        return ny < 0 || nx < 0 || ny >= arr.length || nx >= arr[0].length;
     }
 
 }
