ea6dea5ada108dcf698797da48f823be12c53482
Joochang Park <jooc0311@gmail.com>
2024-07-16 09:45:44 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15544 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,14 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+class Main {
+
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        int[] dp = new int[n + 1];
+        dp = new int[n + 1];
+        // 1 만들 수 있는 최솟값 구하기
         dp[1] = 0;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + 1;
+            dp[i] = dp[i - 1] + 1;
             if (i % 3 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
@@ -17,6 +20,8 @@ public class Main {
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
         }
+
         System.out.println(dp[n]);
     }
+
 }
