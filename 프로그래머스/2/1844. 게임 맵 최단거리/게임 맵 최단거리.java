b3ca6ef471165b1061930a110d78e975b458c182
Joochang Park <jooc0311@gmail.com>
2024-07-29 10:10:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.85 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,57 +1,43 @@
 import java.util.*;
 class Solution {
-    static int[] dx = {0, 1, 0, -1}; 
-    static int[] dy = {1, 0, -1, 0};
-    static boolean[][] visited;
-
+    int[] dx = {0, 1, 0, -1}; 
+    int[] dy = {1, 0, -1, 0};
+    boolean[][] visited;
+    int xl;
+    int yl;
     public int solution(int[][] maps) {
-        int answer = 0;
-        visited = new boolean[maps.length][maps[0].length];
-        bfs(0,0, maps);
-        return maps[maps.length-1][maps[0].length-1] > 1 ? maps[maps.length-1][maps[0].length-1] : -1;
+        yl = maps.length;
+        xl = maps[0].length;
+        visited = new boolean[yl][xl];
+        bfs(0, 0, maps);
+        if(maps[yl - 1][xl - 1] == 1){
+            return -1;
+        }
+        return maps[yl - 1][xl - 1];
     }
-    
-    void bfs(int y, int x, int[][] maps){
-        Queue<int[]> queue= new LinkedList<>();
-        queue.add(new int[] {y, x});
+
+    public void bfs(int y, int x, int[][] map){
+        Queue<int[]> queue = new LinkedList<>();
         visited[y][x] = true;
+        queue.add(new int[]{y, x});
         
         while(!queue.isEmpty()){
-            int[] n = queue.poll();
-            for(int i = 0; i< 4; i++){
-                int ny = n[0] + dy[i];
-                int nx = n[1] + dx[i];
+            int[] loc = queue.poll();
+            
+            for(int i = 0; i < 4; i++){
+                int ny = loc[0] + dy[i];
+                int nx = loc[1] + dx[i];
                 
-                if(ny < 0 || nx < 0 || ny >= maps.length || nx >= maps[0].length) continue;
-                
-                if(maps[ny][nx] != 0 && !visited[ny][nx]){
-                    visited[ny][nx] = true;
-                    maps[ny][nx] = maps[n[0]][n[1]] + 1;
+                if(check(ny, nx) || map[ny][nx] == 0) continue;
+                if(map[ny][nx] == 1 && !visited[ny][nx]){
                     queue.add(new int[]{ny, nx});
+                    map[ny][nx] = map[loc[0]][loc[1]] + 1;
                 }
-                if(ny == maps.length && nx == maps[0].length) break;
             }
         }
-        
     }
-    // void bfs(int i, int j, int[][] maps){
-    //     Queue<int[]> queue = new LinkedList<>();
-    //     queue.add(new int[] {i,j});
-    //     visited[i][j] = true;
-    //     while(!queue.isEmpty()){
-    //         int now[] = queue.poll();
-    //         for(int k = 0; k < 4; k++){
-    //             int x = now[0] + dx[k];
-    //             int y = now[1] + dy[k];
-    //             if(x >= 0 && y >= 0 && x < maps.length && y < maps[0].length){
-    //                 if(maps[x][y] !=0 && !visited[x][y]){
-    //                     visited[x][y] = true;
-    //                     maps[x][y] = maps[now[0]][now[1]] + 1;
-    //                     queue.add(new int[] {x, y});
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
+    
+    public boolean check(int y, int x){
+        return y < 0 || x < 0 || y >= yl || x >= xl;
+    }
 }
