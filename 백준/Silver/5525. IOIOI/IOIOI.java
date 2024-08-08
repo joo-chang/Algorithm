835c267651e017992c0052c7962a3f24c1a5c07b
Joochang Park <jooc0311@gmail.com>
2024-08-08 10:10:49 +0900

    [Silver I] Title: IOIOI, Time: 184 ms, Memory: 20716 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -8,27 +8,25 @@ class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
         int m = Integer.parseInt(bf.readLine());
-        int[] arr = new int[m];
-        String line = bf.readLine();
-        for (int i = 0; i < m; i++) {
-            arr[i] = line.charAt(i) - '0';
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append("IO");
-        }
-        sb.append("I");
-
-        String pattern = sb.toString();
-
+        int answer = 0;
         int count = 0;
-        for (int i = 0; i < m - n * 2; i++) {
-            if (line.substring(i, i + n * 2 + 1).equals(pattern)) {
+        String line = bf.readLine();
+
+        for (int i = 1; i < m - 1; ) {
+            if (line.charAt(i) == 'O' && line.charAt(i + 1) == 'I') {
                 count++;
+                if (count == n) {
+                    if (line.charAt(i - (n * 2 - 1)) == 'I') {
+                        answer++;
+                    }
+                    count--;
+                }
+                i += 2;
+            } else {
+                i++;
+                count = 0;
             }
         }
-        System.out.println(count);
+        System.out.println(answer);
     }
-
-
 }
