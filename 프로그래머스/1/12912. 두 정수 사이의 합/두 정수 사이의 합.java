6b506add3a837cd3240493ca64c8da1a02fe3574
Joochang Park <jooc0311@gmail.com>
2024-08-12 09:46:38 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 33.74 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,13 @@
+import java.util.stream.LongStream;
+class Solution {
+    public long solution(int a, int b) {        
+        long answer = 0;
+        int max = a > b ? a : b;
+        int min = a > b ? b : a;
+        // for(int i = min; i <= max; i++){
+        //     answer += i;
+        // }
+        answer = LongStream.range(min, max + 1).sum();
+        return answer;
+    }
+}
