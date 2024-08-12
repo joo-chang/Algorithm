b192ca049a447aaccbb7a98a7ebfa8aa5625de60
Joochang Park <jooc0311@gmail.com>
2024-08-12 10:00:32 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.12 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,10 +1,13 @@
+import java.util.*;
 class Solution {
     public String solution(String[] seoul) {
         String answer = "";
         int cnt = 0;
+        StringBuilder sb = new StringBuilder();
         for(String s : seoul){
             if(s.equals("Kim")){
-                return "김서방은 " + cnt + "에 있다";
+                // return "김서방은 " + cnt + "에 있다";
+                return sb.append("김서방은 ").append(cnt).append("에 있다").toString();
             }
             cnt++;
         }
