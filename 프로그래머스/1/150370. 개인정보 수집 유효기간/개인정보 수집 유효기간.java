c903c82a4feac62d479841fb2a7242bca4c65e35
Joochang Park <jooc0311@gmail.com>
2024-08-13 12:06:38 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 6.12 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -6,18 +6,18 @@ import java.util.Collections;
 class Solution {
     public ArrayList<Integer> solution(String today, String[] terms, String[] privacies) {
 		ArrayList<Integer> answer = new ArrayList<>();
-        LocalDate todate = LocalDate.parse(today, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        int todate = getDate(today);
         HashMap<String, Integer> exp = new HashMap<>();
         
         for(String t : terms){
             String[] s = t.split(" ");
-            exp.put(s[0], Integer.parseInt(s[1]));
+            exp.put(s[0], Integer.parseInt(s[1]) * 28);
         }
         
         for(int i = 0; i < privacies.length; i++){
             String[] p = privacies[i].split(" ");
-            LocalDate date = LocalDate.parse(p[0], DateTimeFormatter.ofPattern("yyyy.MM.dd")).plusMonths(exp.get(p[1]));
-            if(todate.compareTo(date) >= 0){
+            
+            if(todate >= getDate(p[0]) + exp.get(p[1])){
                 answer.add(i + 1);
             }
         }
@@ -27,4 +27,11 @@ class Solution {
 		return answer;
 	}
 
+    private int getDate(String date){
+        String[] d = date.split("\\.");
+        int year = Integer.parseInt(d[0]) * 12 * 28;
+        int month = Integer.parseInt(d[1]) * 28;
+        int day = Integer.parseInt(d[2]);
+        return year + month + day;
+    }
 }
