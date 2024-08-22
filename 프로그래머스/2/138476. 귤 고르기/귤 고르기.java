a01527a10530d29f2dc09d7a755b585673f0cf5e
Joochang Park <jooc0311@gmail.com>
2024-08-22 09:42:41 +0900

    [level 2] Title: 귤 고르기, Time: 77.11 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        int sum =0;
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int t : tangerine) {
+			map.put(t, map.getOrDefault(t, 0) + 1);
+		}
+		List<Integer> count = new ArrayList<>();
+		for (Integer c : map.keySet()) {
+			count.add(map.get(c));
+		}
+		count.sort(Collections.reverseOrder());
+
+		for (int c : count) {
+			answer++;
+			sum += c;
+			if (sum >= k) {
+				break;
+			}
+		};
+        return answer;
+    }
+}
