54bf52f3d59428f903cfe913c44a53cc31907310
Joochang Park <jooc0311@gmail.com>
2025-04-22 23:51:07 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, date_format(r.created_date, '%Y-%m-%d') CREATED_DATE from used_goods_board b join used_goods_reply r on b.board_id = r.board_id WHERE DATE_FORMAT(B.CREATED_DATE, '%Y-%m') = '2022-10' order by r.created_date, b.title;
